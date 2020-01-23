0a78813ad2cd26cacc34955e9afd362e7ddc499e
Jamir <jamir@live.jp>
2020-01-23 16:59:36 -0300

    update for mongodb deprecation warnings

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ const app = express();
 
 mongoose.connect('mongodb+srv://jamir:werto432@cluster0-8x0st.mongodb.net/app?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 });
-mongoose.set('useCreateIndex', true);
 app.use(express.json())
 app.use(routes);
 app.listen(3333);
